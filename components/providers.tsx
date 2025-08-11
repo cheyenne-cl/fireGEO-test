9a5139b7453630ac93517346e1ec7baf8fd1f6bd
cheyenne-cl <cheyenne@compozelabs.com>
2025-08-11 11:50:34 -0500

    Add Better Auth provider to app providers - fixes authentication issues

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { QueryProvider } from '@/lib/providers/query-provider';
+import { authClient } from '@/lib/auth-client';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryProvider>
-      {children}
-    </QueryProvider>
+    <authClient.Provider>
+      <QueryProvider>
+        {children}
+      </QueryProvider>
+    </authClient.Provider>
   );
 }
